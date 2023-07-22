3c6a36d9407137782d231e7f7ff1c78e9055cba3
Josef Veselý <49988965+JosefVesely@users.noreply.github.com>
2023-07-23 00:41:19 +0200

    Fixed wrong day name. Sunday is first

--- js/time_date.js
+++ js/time_date.js
@@ -32,12 +32,12 @@ function get_suffix(number) {
 
 function date(){
 	var now = new Date();
-    var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     
     var day = now.getDate()
     var suffix = get_suffix(day);
-    var day_name = days[now.getDay()-1];
+    var day_name = days[now.getDay()];
     var month = months[now.getMonth()];
     var year = now.getFullYear();
 
